db730ccb3cd8ae2bfd2cb973e596c096a66cdbe1
Pablo <pablo.bion@hotmaiol.com>
2020-10-06 23:35:16 -0300

    Responsive menu

--- src/components/menu/index.js
+++ src/components/menu/index.js
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-import { Menu } from './styles';
+import { Menu, MenuMobile } from './styles';
 
 
 import HomeIcon from '../../assets/home.svg'
@@ -9,34 +9,53 @@ import GroupIcon from '../../assets/group.svg'
 import AboutIcon from '../../assets/about.svg'
 
 function MenuComponent() {
+
+  const [widthscreen, setWidthscreen] = useState(window.screen.width);
+
+
   return (
     <>
-      <Menu>
-        <button>
-        <div className='cell'>
-          <img className='icon' src={HomeIcon} alt="" />
-          <p>Inicio</p>
-        </div>
-        </button>
-        <button>
-        <div className='cell'>
-          <img className='icon' src={FakeIcon} alt="" />
-          <p>Detector</p>
-        </div>
-        </button>
-        <button>
-        <div className='cell'>
-          <img className='icon' src={GroupIcon} alt="" />
-          <p>Grupos</p>
-        </div>
-        </button>
-        <button>
-        <div className='cell'>
-          <img className='icon' src={AboutIcon} alt="" />
-          <p>Sobre Nós</p>
-        </div>
-        </button>
-      </Menu>
+      {widthscreen < 500 && // Só renderiza se for tela de pc
+        <MenuMobile>
+
+
+          <input id="check" type="checkbox" />
+          <label for="check">
+            <div class="menu-hamb">
+              <span class="hamburguer"></span>
+            </div>
+          </label>
+
+          <ul class="ul-mbl">
+            <div><img className='icon' src={FakeIcon} alt="" /><a href='#'>Detector</a></div>
+            <div><img className='icon' src={GroupIcon} alt="" /><a href='#'>Grupos</a></div>
+            <div><img className='icon' src={AboutIcon} alt="" /><a href='#'>Sobre Nós</a></div>
+            
+          </ul>
+        </MenuMobile>
+      }
+      {widthscreen >= 500 && // Só renderiza se for tela de pc
+        <Menu>
+          <button>
+            <div className='cell'>
+              <img className='icon' src={FakeIcon} alt="" />
+              <p>Detector</p>
+            </div>
+          </button>
+          <button>
+            <div className='cell'>
+              <img className='icon' src={GroupIcon} alt="" />
+              <p>Grupos</p>
+            </div>
+          </button>
+          <button>
+            <div className='cell'>
+              <img className='icon' src={AboutIcon} alt="" />
+              <p>Sobre Nós</p>
+            </div>
+          </button>
+        </Menu>
+      }
     </>
 
   )
--- src/components/menu/styles.js
+++ src/components/menu/styles.js
@@ -1,10 +1,137 @@
 import styled from 'styled-components'
 
+export const MenuMobile = styled.div`
+
+height: 50px;
+input{
+  display:none;
+}
+
+.menu-hamb {
+      
+        width: 100vw;
+        height: 80px;
+        position: absolute;
+        transition: .5s ease-in-out;
+    }
+
+
+input:checked ~ label .menu-hamb {
+        background: #fff;
+        width: 100vw;
+        height: 100vh;
+        position:fixed;
+    }
+
+.hamburguer{
+  display:block;
+  width:40px;
+  height:5px;
+  border-radious: 2px;
+  background:#333;
+  position:relative;
+  left:5%;
+  top:30px;
+  transition: .5s ease-in-out;
+}
+
+.hamburguer:after,
+.hamburguer:before{
+  width:100%;
+  height:100%;
+  content:'';
+  background:#333;
+  position:absolute;
+  transition: .5s ease-in-out;
+}
+
+.hamburguer:before{
+  top:-10px;
+}
+
+.hamburguer:after{
+  bottom:-10px;
+}
+
+  input:checked ~ label .hamburguer {
+        transform: rotate(45deg);
+    }
+
+    input:checked ~ label .hamburguer:before {
+        transform: rotate(90deg);
+        top: 0;
+    }
+
+    input:checked ~ label .hamburguer:after {
+        transform: rotate(90deg);
+        bottom: 0;
+    }
+
+input:checked ~ .ul-mbl {
+  opacity:1;
+}
+
+.ul-mbl {
+   position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    list-style: none;
+    font-size: 20px;
+    opacity: 0;
+    transition: .25s .1s ease-in-out;
+    z-index: 1
+
+    
+}
+
+.ul-mbl div {
+  display: flex;
+  justify-content: center;
+  margin-bottom: 30px;
+
+  a {
+    margin-left: 20px;
+  }
+}
+
+.ul-mbl img {
+  width: 50px;
+}
+
+
+
+.ul-mbl li{
+  margin:40px auto;
+}
+.ul-mbl a{
+  color: #333;
+  text-transform: uppercase;
+  text-decoration: none;
+  margin: 10px auto;
+
+}
+`
+
+
+
+
+
+
+
+
+
+
+
+
 export const Menu = styled.div`
    
     height: 60px;
     display: flex;
-    padding: 5px 5px 10px 50px;
+    
+    @media (min-width: 500px){ 
+      padding: 5px 5px 10px 50px;
+    }
 
     button {
       border: none;
@@ -21,8 +148,22 @@ export const Menu = styled.div`
         overflow: hidden;
         margin-left: 20px;
 
+        @media (min-width: 500px){ 
+          width: 80px;
+          height: 50px;
+          display: flex;
+          align-items: center;
+          transition: width 1s;
+          text-overflow: clip;
+          overflow: hidden;
+          margin-left: 20px;
+        }
+
         .icon{
-          width: 50px;
+          width: 35px;
+          @media (min-width: 500px){ 
+            width: 50px;
+          }
         }
         p{
           padding-left: 30px;
