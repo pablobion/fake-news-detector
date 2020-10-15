362a9975364909b1558676b82e86f1c418c92235
Pablo Bion <pablo.bion@hotmail.com>
2020-10-15 00:12:36 -0300

    Add: Components group page

--- src/pages/groups/index.js
+++ src/pages/groups/index.js
@@ -1,4 +1,4 @@
-import React, { useReducer } from "react";
+import React, { useReducer, useEffect } from "react";
 
 import { Container } from "./styles";
 
@@ -27,6 +27,7 @@ const Groups = () => {
         try {
             const response = await fetch(`https://tcspedroverani.herokuapp.com/group/view?=user${user}`, settings);
             const data = response.json();
+
             //Isso tem que ser executado assim que o usuário abrir a página de groups, se não vir nada é pq ele não ta em nenhum grupo.
             //Vai retornar dados do grupo, fazer um campo pro nome do grupo, descricao, participantes, e um botão pra sair do grupo.
             //Caso o usuário não tenha grupo, exibir um botão para ele criar um grupo.
@@ -59,6 +60,10 @@ const Groups = () => {
         //tem que ver o quão custoso é deixar o grupo editável.
     };
 
+    useEffect(() => {
+        getGroup();
+    }, []);
+
     const [userInput, setUserInput] = useReducer(
         //States da pagina
         (state, newState) => ({ ...state, ...newState }),
@@ -78,7 +83,7 @@ const Groups = () => {
         <>
             <Menu />
             <Container>
-                {userInput.mode === "create" && (
+                {userInput.mode === "nogroup" && (
                     <>
                         <h1 className="description">Crie um grupo reuna seus amigos e familiares e mantenha todos informados sobre as fake news</h1>
 
@@ -89,6 +94,48 @@ const Groups = () => {
                             <div id="groupimage">
                                 <img src={GroupImg} alt="" />
                                 <h1 id="text-bottom-button">Crie agora mesmo</h1>
+                            </div>
+                        </div>
+                    </>
+                )}
+                {userInput.mode === "create" && (
+                    <>
+                        <div id="create-group">
+                            <h1 id="title-create-group">Crie seu grupo</h1>
+                            <div id="div-form-create-group">
+                                <div id="div-group-name">
+                                    <p>Nome do grupo</p>
+                                    <input></input>
+                                </div>
+                                <div id="div-description-group">
+                                    <p>Descrição do grupo</p>
+                                    <textarea id="description-group" />
+                                </div>
+                                <div id="div-list-emails">
+                                    <h2>Adicione e-mails a lista do grupo</h2>
+                                    <div id="div-list-emails-inside">
+                                        <div>
+                                            <input id="input-email" />
+                                            <button id="button-add-email">
+                                                <FaPlus />
+                                            </button>
+                                        </div>
+
+                                        <ul>
+                                            <li>pablo.bion@hotmail.com</li>
+                                            <li>pablo.bion@hotmail.com</li>
+                                            <li>pablo.bion@hotmail.com</li>
+                                            <li>pablo.bion@hotmail.com</li>
+                                            <li>pablo.bion@hotmail.com</li>
+                                            <li>pablo.bion@hotmail.com</li>
+                                            <li>pablo.bion@hotmail.com</li>
+                                            <li>pablo.bion@hotmail.com</li>
+                                            <li>pablo.bion@hotmail.com</li>
+                                            <li>pablo.bion@hotmail.com</li>
+                                            <li>pablo.bion@hotmail.com</li>
+                                        </ul>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </>
--- src/pages/groups/styles.js
+++ src/pages/groups/styles.js
@@ -72,4 +72,127 @@ export const Container = styled.div`
             }
         }
     }
+
+    #create-group {
+        color: #454545;
+        width: 100vw;
+        #title-create-group {
+            font-size: 30px;
+        }
+
+        #div-form-create-group {
+            display: flex;
+            align-items: center;
+            flex-direction: column;
+            #div-group-name {
+                display: flex;
+                align-items: center;
+                border: 1px solid #00b0ff;
+                border-radius: 6px 6px 6px 6px;
+                margin-top: 20px;
+                p {
+                    font-size: 18px;
+                    background-color: #00b0ff;
+                    padding: 10px;
+                    border-radius: 5px 0px 0px 5px;
+                    color: white;
+                }
+                input {
+                    margin-left: 10px;
+                    width: 200px;
+                    height: 30px;
+                    border: none;
+                    font-size: 18px;
+                    padding-right: 5px;
+                }
+            }
+
+            #div-description-group {
+                margin-top: 30px;
+                border: 1px solid #00b0ff;
+
+                border-radius: 10px 10px 10px 10px;
+                p {
+                    font-size: 18px;
+                    border-radius: 8px 8px 0px 0px;
+                    background-color: #00b0ff;
+                    padding: 10px;
+                    color: white;
+                }
+
+                textarea {
+                    border: none;
+                    resize: none;
+                    width: 355px;
+                    height: 150px;
+                    padding: 10px;
+                    background: rgba(255, 255, 255, 0.1); /* deixa o fundo branco transparente */
+                    backdrop-filter: blur(1px);
+                    cursor: text;
+                }
+
+                textarea::-webkit-scrollbar {
+                    width: 12px;
+                    background-color: #f5f5f5;
+                }
+
+                textarea::-webkit-scrollbar-thumb {
+                    border-radius: 10px;
+                    -webkit-box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.1);
+                    background-color: #4285f4;
+                }
+            }
+
+            #div-list-emails {
+                margin-top: 30px;
+                #div-list-emails-inside {
+                    border: 1px solid lightgray;
+                    border-style: none solid solid solid;
+                    border-radius: 10px;
+
+                    div {
+                        display: flex;
+                        align-items: flex-start;
+                    }
+
+                    input {
+                        font-size: 17px;
+                        padding: 10px;
+                        border: 1px solid #00b0ff;
+                        border-radius: 10px 0px 0px 10px;
+                    }
+                    button {
+                        font-size: 18px;
+                        padding: 10px;
+                        border: none;
+                        color: white;
+                        background-color: #00b0ff;
+                        border-radius: 0px 8px 8px 0px;
+                        &:hover {
+                            background: ${shade(0.1, "#00B0FF")};
+                        }
+                    }
+
+                    ul {
+                        list-style-type: none;
+                        font-size: 14px;
+                        margin-bottom: 5px;
+
+                        li {
+                            padding: 5px;
+                            border-bottom: 1px solid lightgray;
+                            border-top: 1px solid lightgray;
+                        }
+                        li:first-child {
+                            border-top: none;
+                            margin-top: 2px;
+                        }
+                        li:last-child {
+                            border-bottom: none;
+                        }
+                    }
+                }
+            }
+        }
+    }
 `;
