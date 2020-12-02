c06f7ce73655ba1b0f576ab20ab3b24c43dbc4ac
Pablo Bion <50814036+pablobion@users.noreply.github.com>
2020-12-01 23:05:12 -0300

    change select suggestions page

--- src/pages/Suggestions/styles.js
+++ src/pages/Suggestions/styles.js
@@ -64,6 +64,14 @@ export const Container = styled.div`
             select {
                 height: 40px;
                 font-size: 16px;
+    -webkit-padding-end: 20px;
+    -moz-padding-end: 20px;
+    -webkit-padding-start: 20px;
+    -moz-padding-start: 20px;
+    background-color: SkyBlue; /* Fallback color if gradients are not supported */
+    color: white;
+    border: none;
+                
                 @media (min-width: 700px){
                     margin-right: 20px;
                     width: 20vw;
@@ -72,6 +80,10 @@ export const Container = styled.div`
                     margin-bottom: 50px;
                     width: 80vw;
                 }    
+
+                option {
+                    background: gray;
+                }
             }
             
 
