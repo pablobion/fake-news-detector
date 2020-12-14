5be09c0f05ac1f2df8548f597d0498adb461131c
Pablo Bion <50814036+pablobion@users.noreply.github.com>
2020-12-14 19:51:06 -0300

    Count email and news (home page)

--- src/pages/login/switchPages/login/index.js
+++ src/pages/login/switchPages/login/index.js
@@ -48,21 +48,6 @@ function SwitchPages(props) {
         }
     };
 
-    useEffect(() => {
-        (async () => {
-            try {
-                const response = await fetch("https://tcspedroverani.herokuapp.com/report/extract");
-                const data = await response.json();
-                console.log(data);
-            } catch (error) {}
-            //isso é a função pra pegar os dados que vão ser exibidos quando o usuário NÃO estiver logado
-            //Já enviamos X notícias, e já enviamos mais de X emails avisando colegas.
-
-            //não sei se vai exatamente nesse arquivo, porque no criação de conta e recuperação de senha também vai exibir,
-            //n sei se teria que fazer 1 useEffect em cada arquivo..
-        })();
-    }, []);
-
     return (
         <>
             <LinkCreateAccount>
