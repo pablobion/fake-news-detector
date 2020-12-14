cf570399294519dba5f3e8a1be4db0afc923f3c3
Pablo Bion <50814036+pablobion@users.noreply.github.com>
2020-12-14 20:01:34 -0300

    Css about page

--- src/pages/about/styles.js
+++ src/pages/about/styles.js
@@ -7,4 +7,20 @@ export const Container = styled.div`
     align-items: center;
     font-size: 18px;
     color: #454545;
+    flex-direction: column;
+    margin: 50px;
+
+    h1 {
+        margin-bottom: 10px;
+        margin-top: 40px;
+    }
+
+    h2 {
+        margin-top: 30px;
+        margin-bottom: 10px;
+    }
+
+    p {
+        width: 60vw;
+    }
 `;
