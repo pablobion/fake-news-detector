b2bb6b2e3bc567d67391467178427de6d883f7ef
Pablo Bion <50814036+pablobion@users.noreply.github.com>
2020-12-15 22:47:06 -0300

    Fix function send suggestion

--- src/pages/Suggestions/index.js
+++ src/pages/Suggestions/index.js
@@ -1,14 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import Menu from "../../components/menu/";
 import { Container } from "./styles";
+import { useAlert, types } from "react-alert";
+
+import { useHistory } from "react-router-dom";
 
 const Suggestions = () => {
+    const alert = useAlert();
+    const history = useHistory();
+    const [content, setContent] = useState("");
+
+    const handleChange = (evt) => {
+        const event = evt.target.value;
+        setContent(event);
+    };
+
     const sendSuggestion = async () => {
         const user = localStorage.getItem("user");
         const authorization = localStorage.getItem("qwert");
 
         const settings = {
             method: "POST",
+            body: JSON.stringify({ user, content }),
             headers: {
                 Accept: "application/json",
                 "Content-Type": "application/json",
@@ -20,7 +33,8 @@ const Suggestions = () => {
             const response = await fetch("https://cors-anywhere.herokuapp.com/tcspedroverani.herokuapp.com/suggestion/suggest", settings);
             const data = await response.json();
             if (data.success) {
-                //aqui printar o data.message
+                alert.show("Sugestão enviada com sucesso.", { type: types.SUCCESS });
+                setContent("");
             }
         } catch (error) {
             //houve um erro ao realizar a sugestão, tente novamente mais tarde
@@ -33,7 +47,7 @@ const Suggestions = () => {
             <Container>
                 <h1>Sugestão</h1>
                 <p id="title">Digite aqui uma noticia que você desja sugerir para aprimorarmos a nossa inteligencia artificial. Encaminhando o texto e tipo.</p>
-                <textarea />
+                <textarea value={content} onChange={handleChange} />
                 <div>
                     <select id="input-select">
                         <option value="" disabled selected hidden>
@@ -42,7 +56,9 @@ const Suggestions = () => {
                         <option value="true">Verdadeira</option>
                         <option value="false">Falsa</option>
                     </select>
-                    <button type="submit">Enviar</button>
+                    <button type="submit" onClick={sendSuggestion}>
+                        Enviar
+                    </button>
                 </div>
             </Container>
         </>
